446f54a347c58b6689159c3de8a10f56083929fa
刘永强 <nikbobo@vip.qq.com>
2021-05-14 20:03:57 +0800

    Update op-diy-part2.sh

--- op-diy-part2.sh
+++ op-diy-part2.sh
@@ -15,4 +15,4 @@
 sed -i 's/^\(PKG_VERSION:=\)\(.*\)/\14.27.0/' feeds/lienol/v2ray/Makefile
 sed -i 's/^\(PKG_HASH:=\)\(.*\)/\15e1b0146953398a5422611b20f5e62c3a9c3efd62cd42295762f6f39d8eff31e/' feeds/lienol/v2ray/Makefile
 sed -i 's/$(STAGING_DIR_HOST)\/bin\/upx --lzma --best $(GO_PKG_BUILD_BIN_DIR)\/v2ray-plugin//' feeds/lienol/v2ray-plugin/Makefile
-sed -i 's/ln_start_bin "$(first_type $(config_t_get global_app xray_file) xray)" xray $log_file -config="$config_file"/ln_start_bin "$(first_type $(config_t_get global_app xray_file) v2ray)" v2ray $log_file -config="$config_file"/' feeds/lienol/luci-app-passwall/root/usr/share/passwall/app.sh
+sed -i 's/ln_start_bin "$(first_type $(config_t_get global_app xray_file) xray)" xray $log_file -config="$config_file"/ln_start_bin "$(first_type $(config_t_get global_app v2ray_file) v2ray)" v2ray $log_file -config="$config_file"/' feeds/lienol/luci-app-passwall/root/usr/share/passwall/app.sh
