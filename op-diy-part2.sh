193c796d5076df9d2d580e42b995d1c67a02da81
刘永强 <nikbobo@vip.qq.com>
2021-05-14 20:29:04 +0800

    Update op-diy-part2.sh

--- op-diy-part2.sh
+++ op-diy-part2.sh
@@ -14,5 +14,7 @@
 # sed -i 's/192.168.1.1/192.168.50.5/g' package/base-files/files/bin/config_generate
 sed -i 's/^\(PKG_VERSION:=\)\(.*\)/\14.27.0/' feeds/lienol/v2ray/Makefile
 sed -i 's/^\(PKG_HASH:=\)\(.*\)/\15e1b0146953398a5422611b20f5e62c3a9c3efd62cd42295762f6f39d8eff31e/' feeds/lienol/v2ray/Makefile
-sed -i 's/$(STAGING_DIR_HOST)\/bin\/upx --lzma --best $(GO_PKG_BUILD_BIN_DIR)\/v2ray-plugin//' feeds/lienol/v2ray-plugin/Makefile
+# sed -i 's/$(STAGING_DIR_HOST)\/bin\/upx --lzma --best $(GO_PKG_BUILD_BIN_DIR)\/v2ray-plugin//' feeds/lienol/v2ray-plugin/Makefile
 sed -i 's/ln_start_bin "$(first_type $(config_t_get global_app xray_file) xray)" xray $log_file -config="$http_config_file"/ln_start_bin "$(first_type $(config_t_get global_app v2ray_file) v2ray)" v2ray $log_file -config="$http_config_file"/' feeds/lienol/luci-app-passwall/root/usr/share/passwall/app.sh
+sed -i '/tools-y += patchelf pkgconf quilt squashfskit4 sstrip zip zlib zstd/atools-y += ucl upx/' tools/Makefile
+sed -i '/$(curdir)\/autoconf\/compile := $(curdir)\/m4\/compile/i$(curdir)\/upx\/compile := $(curdir)\/ucl\/compile/' tools/Makefile
